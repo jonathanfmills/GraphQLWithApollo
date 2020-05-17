8ecaf8e88f69cb5314d2e73df69b0e1a8891f31b
Jonathan Mills <jonathanfmills@gmail.com>
2020-05-17 16:56:30 -0500

    end m2C4

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ type Session {
   room:String,
   day:String,
   format: String,
-  track:String,
+  track:String @deprecated(reason: "Too many sessions do not fit into a single track, we will be migrating to a tags based system in the future..."),
   level:String
 }`
 
