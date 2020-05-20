b65e54e834808e3c7705a33c7d71b7f0cc7491ef
Jonathan Mills <jonathanfmills@gmail.com>
2020-05-20 17:36:02 -0500

    end m2c6

--- datasources/sessions.js
+++ datasources/sessions.js
@@ -1,17 +1,21 @@
-const sessions = require('../data/sessions.json');
-const { DataSource } = require('apollo-datasource');
+const sessions = require("../data/sessions.json");
+const { DataSource } = require("apollo-datasource");
+const _ = require("lodash");
 
 class SessionAPI extends DataSource {
   constructor() {
     super();
   }
 
-  initialize(config) {
-
-  }
+  initialize(config) {}
 
   getSessions() {
     return sessions;
+  }
+
+  getSessionById(id) {
+    const session = _.filter(sessions, { id: parseInt(id) });
+    return session[0];
   }
 }
 
--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-const { ApolloServer, gql } = require('apollo-server');
-const SessionAPI = require('./datasources/sessions');
+const { ApolloServer, gql } = require("apollo-server");
+const SessionAPI = require("./datasources/sessions");
 
 const typeDefs = gql`
-type Query {
-  sessions:[Session]
-}
-type Session {
-  id: ID!
-  title: String!,
-  description:String,
-  startsAt:String,
-  endsAt:String,
-  room:String,
-  day:String,
-  format: String,
-  track:String @deprecated(reason: "Too many sessions do not fit into a single track, we will be migrating to a tags based system in the future..."),
-  level:String
-}`
+  type Query {
+    sessions: [Session]
+    sessionById(id: ID): Session
+  }
+  type Session {
+    id: ID!
+    title: String!
+    description: String
+    startsAt: String
+    endsAt: String
+    room: String
+    day: String
+    format: String
+    track: String
+      @deprecated(
+        reason: "Too many sessions do not fit into a single track, we will be migrating to a tags based system in the future..."
+      )
+    level: String
+  }
+`;
 
 const resolvers = {
   Query: {
     sessions: (parent, args, { dataSources }, info) => {
       return dataSources.sessionAPI.getSessions();
-    }
-  }
+    },
+    sessionById: (parent, { id }, { dataSources }, info) => {
+      return dataSources.sessionAPI.getSessionById(id);
+    },
+  },
 };
 
 const dataSources = () => ({
-  sessionAPI: new SessionAPI()
+  sessionAPI: new SessionAPI(),
 });
 const server = new ApolloServer({ typeDefs, resolvers, dataSources });
 
-server
-  .listen({ port: process.env.PORT || 4000 })
-  .then(({ url }) => {
-    console.log(`graphQL running at ${url}`);
-  })
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`graphQL running at ${url}`);
+});
