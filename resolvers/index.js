02e283e5debd5d0a7a49103ec10e9b48750880be
Mat Warger <mwarger@gmail.com>
2020-06-03 22:24:25 -0500

    mutation to mark speaker as featured

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,5 +1,6 @@
 const Query = require('./query');
+const Mutation = require('./mutation');
 const Session = require('./sessions');
-const resolvers = { Query, Session };
+const resolvers = { Query, Mutation, Session };
 
 module.exports = resolvers;
--- speakers/routes/speakers.js
+++ speakers/routes/speakers.js
@@ -1,41 +1,38 @@
 var express = require('express');
 var router = express.Router();
-var speakers = require('../data/speakers')
+var speakers = require('../data/speakers');
 var _ = require('lodash');
 
 router.use('/:id', (req, res, next) => {
-  let speaker = _.filter(speakers, {id:req.params.id})[0]
-    if (speaker) {
-      req.speaker = speaker;
-      return next();
-    }
-    return res.sendStatus(404);
-  });
+  let speaker = _.filter(speakers, { id: req.params.id })[0];
+  if (speaker) {
+    req.speaker = speaker;
+    return next();
+  }
+  return res.sendStatus(404);
+});
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.json(speakers);
 });
 
-router.post('/', function(req, res, next){
-  res.json({'message': 'not implemented'})
+router.post('/', function (req, res, next) {
+  res.json({ message: 'not implemented' });
 });
 
-router.patch('/:id', function(req, res, next){
-  
+router.patch('/:id', function (req, res, next) {
+  let speaker = _.filter(speakers, { id: req.params.id })[0];
   Object.entries(req.body).forEach((item) => {
     const key = item[0];
     const value = item[1];
-    req.speaker[key] = value;
-    console.log(req.speaker);
+    speaker[key] = value;
   });
-  
-  res.json(req.speaker);
-  
+
+  res.json(speaker);
 });
 
-router.get('/:id', function(req, res, next) {
-    res.json(_.filter(speakers, {id:req.params.id})[0]);
+router.get('/:id', function (req, res, next) {
+  res.json(_.filter(speakers, { id: req.params.id })[0]);
 });
-
 
 module.exports = router;
